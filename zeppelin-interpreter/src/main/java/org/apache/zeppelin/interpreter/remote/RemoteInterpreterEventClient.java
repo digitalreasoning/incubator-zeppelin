3bc9587e6b020d639f47755bbd0073ae22f84d13
Chris Reynolds <christian.reynolds@digitalreasoning.com>
2017-03-16 09:13:56 -0500

    Fixed the onAppend large output problem

--- zeppelin-interpreter/src/main/java/org/apache/zeppelin/interpreter/remote/RemoteInterpreterEventClient.java
+++ zeppelin-interpreter/src/main/java/org/apache/zeppelin/interpreter/remote/RemoteInterpreterEventClient.java
@@ -216,18 +216,33 @@ public class RemoteInterpreterEventClient implements ResourcePoolConnector {
     Map<String, String> appendOutput = new HashMap<String, String>();
     appendOutput.put("noteId", noteId);
     appendOutput.put("paragraphId", paragraphId);
-    appendOutput.put("data", output);
+    String message = output;
+    if (message.length() > getParagraphMaxOutput())
+    {
+      message = "Output too large. Writing to file instead";
+    }
+    appendOutput.put("data", message);
 
     sendEvent(new RemoteInterpreterEvent(
         RemoteInterpreterEventType.OUTPUT_APPEND,
         gson.toJson(appendOutput)));
   }
 
+  private int getParagraphMaxOutput()
+  {
+    return Integer.parseInt(System.getenv(RemoteInterpreterConfig.PARAGRAPH_MAX_OUTPUT_KEY));
+  }
+
   public void onInterpreterOutputUpdate(String noteId, String paragraphId, String output) {
     Map<String, String> appendOutput = new HashMap<String, String>();
     appendOutput.put("noteId", noteId);
     appendOutput.put("paragraphId", paragraphId);
-    appendOutput.put("data", output);
+    String message = output;
+    if (message.length() > getParagraphMaxOutput())
+    {
+      message = "Output too large. Writing to file instead";
+    }
+    appendOutput.put("data", message);
 
     sendEvent(new RemoteInterpreterEvent(
         RemoteInterpreterEventType.OUTPUT_UPDATE,
